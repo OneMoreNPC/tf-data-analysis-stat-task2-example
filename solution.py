e52b0d3ae0178f1bc43b722c15dc56a64d21296e
OneMoreNPC <129788997+OneMoreNPC@users.noreply.github.com>
2023-04-06 23:55:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import numpy as np
-
 from scipy.stats import chi2 
-
 
 chat_id = 49479018 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x) 
-    alpha = 1 - p 
-    sqr_sum = 0 
-    for i in range(n): 
-        sqr_sum = sqr_sum + x[i]**2 
-    left_i = sqr_sum / chi2.ppf((1-alpha / 2), df = 2*n) 
-    right_i = sqr_sum / chi2.ppf((alpha / 2), df = 2*n) 
-    return np.sqrt(left_i/2) , \ 
-           np.sqrt(right_i/2)
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    n = len(x)
+    alpha = 1 - p
+    loc = x.mean()
+    sqr_s = 0
+    for i in range(n):
+        sqr_s += x[i]**2
+    left = sqr_s / chi2.ppf((1-alpha / 2), df = 2*n)
+    right = sqr_s / chi2.ppf((alpha / 2), df = 2*n)
+    return np.sqrt(left/2) , \
+           np.sqrt(right/2)
