656f44cc8b92dd819c7318a42a09eb7977b145e0
OneMoreNPC <129788997+OneMoreNPC@users.noreply.github.com>
2023-04-06 22:13:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,8 +11,8 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p 
     sqr_sum = 0 
     for i in range(n): 
-        sq_sum = sq_sum + x[i]**2 
-    left_i = sq_sum / chi2.ppf((1-alpha / 2), df = 2*n) 
-    right_i = sq_sum / chi2.ppf((alpha / 2), df = 2*n) 
+        sqr_sum = sqr_sum + x[i]**2 
+    left_i = sqr_sum / chi2.ppf((1-alpha / 2), df = 2*n) 
+    right_i = sqr_sum / chi2.ppf((alpha / 2), df = 2*n) 
     return np.sqrt(left_i/2) , \ 
            np.sqrt(right_i/2)
